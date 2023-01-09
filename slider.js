0e82b4ad5e50cbafd8b6ab16e3a366e7bce6106d
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2023-01-10 01:43:15 +0600

    Global Changes

--- slider.js
+++ slider.js
@@ -22,33 +22,34 @@
 //   })
 // })
 
-let slides = document.querySelectorAll('.slide');
-let dots = document.querySelectorAll('dot');
-let index = 0;
 
-function prevSlide(n) {
-  index += n;  
-  console.log("prevSlide is called");
-  changeSlide();
-}
+// let slides = document.querySelectorAll('.slide');
+// let dots = document.querySelectorAll('dot');
+// let index = 0;
 
-function nextSlide(n) {
-  index += n;
-  changeSlide();
-}
+// function prevSlide(n) {
+//   index += n;  
+//   console.log("prevSlide is called");
+//   changeSlide();
+// }
 
-function changeSlide() {
-  if (index > slides.length - 1)
-    index = 0;
-  if (index < 0)
-    index = slides.length - 1;
+// function nextSlide(n) {
+//   index += n;
+//   changeSlide();
+// }
+
+// function changeSlide() {
+//   if (index > slides.length - 1)
+//     index = 0;
+//   if (index < 0)
+//     index = slides.length - 1;
   
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
+//     for (let i = 0; i < slides.length; i++) {
+//       slides[i].style.display = 'none';
 
-      dots[i].classList.remove('active');
+//       dots[i].classList.remove('active');
       
-    }
-  slides[index].style.display = 'block';
-  dots[index].classList.add('active');
-}
+//     }
+//   slides[index].style.display = 'block';
+//   dots[index].classList.add('active');
+// }
