7f68b930b8cd91b55b05b89fb637cba8edc6335c
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2023-01-12 01:00:20 +0600

    steps

--- main.js
+++ main.js
@@ -108,36 +108,11 @@ if (menu && menuBtn) {
     })
 }
 
-//map
+//steps
+// const step_wizard_item = document.querySelectorAll('step_wizard_item');
 
-// google.maps.event.addDomListener(window, 'load', init);  
-        
-            // function init() {
-            //     // Basic options for a simple Google Map
-            //     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-            //     var mapOptions = {
-            //         // How zoomed in you want the map to start at (always required)
-            //         zoom: 11,
-
-            //         // The latitude and longitude to center the map (always required)
-            //         center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-            //         // How you would like to style the map. 
-            //         // This is where you would paste any style found on Snazzy Maps.
-            //         styles: [{"featureType":"all","elementType":"labels.text","stylers":[{"color":"#878787"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f9f5ed"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"color":"#f5f5f5"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#aee0f4"}]}]
-            //     };
-
-            //     // Get the HTML DOM element that will contain your map 
-            //     // We are using a div with id="map" seen below in the <body>
-            //     var mapElement = document.querySelector('.js-map');
-
-            //     // Create the Google Map using our element and options defined above
-            //     var map = new google.maps.Map(mapElement, mapOptions);
-
-            //     // Let's also add a marker while we're at it
-            //     var marker = new google.maps.Marker({
-            //         position: new google.maps.LatLng(40.6700, -73.9400),
-            //         map: map,
-            //         title: 'Snazzy!'
-            //     });
-            // }
+// for (i = 1; i < step_wizard_item.length; i++) {
+//     step_wizard_item[i].addEventListener('click', function () {
+//         this.classList.remove('current_item');
+//     });
+// };
