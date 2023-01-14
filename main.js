4a3cb7364d71b2e1feb133897029044a315a5e97
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2023-01-14 22:34:19 +0600

    slider

--- main.js
+++ main.js
@@ -148,3 +148,19 @@ function updated3() {
         clearInterval(counts3);
     }
 };
+//slider
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     slidesPerGroup: 3,
+//     loop: true,
+//     loopFillGroupWithBlank: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
